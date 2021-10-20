d400d47a8212fdddf5e63ec13c7690ae1a42c144
Luciano Pongeluppi <lucianopongeluppi@gmail.com>
2021-10-19 21:37:53 -0300

    Exercicio 1 corrigido

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,25 +1,63 @@
 import React, { Component } from 'react';
-import { CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardTitle, CardBody } from 'reactstrap';
 
 class DishDetail extends Component {
 
-    render() {
-        const comment = this.props.dish.map((comment) => {
-            return (
-                <div key={comment.id}>
-                    <CardText>{comment.comment}</CardText>
-                    <CardText>-- {comment.author}, {comment.date}</CardText>
-                    <CardText>{comment.comment}</CardText>
+    renderDish(dish) {
+        return (
+            <div className="col-12 col-md-5 m-1">
+                <Card>
+                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle style={{ fontWeight: 'bold', fontSize: 20 }}>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+    }
+
+    renderComments(comments){
+        if (comments != null){
+            return(
+                <div className="col-12 col-md-5 m-1">
+                    <h4>Comments</h4>
+                    <ul className="list-unstyled">
+                        {comments.map((comment) => {
+                            return (
+                                <li key={comment.id}>
+                                    <p>{comment.comment}</p>
+                                    <p>-- {comment.author}, 
+                                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                                    </p>
+                                </li>
+                            )
+                        })}
+                    </ul>
                 </div>
             );
-        })
+        }else{
+            return(
+                <div></div>
+            );
+        }
+    }
 
-        return (
-            <div>
-                <CardTitle style={{ fontWeight: 'bold', fontSize: 30 }}>Comments</CardTitle>
-                {comment}
-            </div>
-        )
+    render() {
+        if(this.props.dish != null){
+            return (
+                <div className="container">
+                    <div className="row">
+                        {this.renderDish(this.props.dish)}
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
+                </div>
+            );
+        }else{
+            return(
+                <div></div>
+            );
+        }
     }
 }
 
