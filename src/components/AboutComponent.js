b7211bc7fcea13f2759ddafbb08eece8408fb0a2
unknown <lucianopongeluppi@gmail.com>
2021-10-25 14:32:52 -0300

    Exercicio 4

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({ leader }) {
     return (
         <Media tag='li'>
             <Media left middle>
-                <Media object src={leader.image} alt={leader.name} />
+                <Media object src={baseUrl + leader.image} alt={leader.name} />
             </Media>
             <Media right body className="ml-5">
                 <Media heading>{leader.name}</Media>
@@ -19,19 +22,37 @@ function RenderLeader({ leader }) {
 }
 
 function LeaderList(props) {
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
-            <div className="col-12 mt-2">
-                <RenderLeader leader={leader} />
-            </div>
+            <Fade in key={leader._id}>
+                <div className="col-12 mt-2">
+                    <RenderLeader leader={leader} />
+                </div>
+            </Fade>
         );
     });
 
-    return (
-        <Media list>
-            {leaders}
-        </Media>
-    );
+    if (props.leaders.isLoading) {
+        return (
+            <Loading />
+        );
+    }
+    else if (props.leaders.errMess) {
+        return (
+            <div className="col-12">
+                <h4>{props.leaders.errMess}</h4>
+            </div>
+        );
+    }
+    else {
+        return (
+            <Media list>
+                <Stagger in>
+                    {leaders}
+                </Stagger>
+            </Media>
+        );
+    }
 }
 
 function About(props) {
